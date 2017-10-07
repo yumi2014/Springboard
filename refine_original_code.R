aca408dcd834310cfd84ea6c3284369d078bdbbd
Yumi <ychoi@gwu.edu>
2017-10-07 10:29:17 -0400

    titanic original code

--- refine_original_code.R
+++ refine_original_code.R
@@ -22,9 +22,24 @@ setwd("/Users/yumichoi/Desktop/Springboard")
  
  # 5: Create dummy variables for company and product category - company part is done, product category part was incomplete due to # 3
 
- rawdata$company_philips <- ifelse(rawdata$company=="philips" & rawdata$number>=5, 1, 0)
+
  rawdata$company_philips <- ifelse(rawdata$company=="philips" & rawdata$number>=34, 1, 0)
  rawdata$company_philips <- ifelse(rawdata$company=="philips" & rawdata$number>=5, 1, 0)
 
  # 6: Submit the project on Github
  write.csv(rawdata, 'refine_clean.csv')
+ 
+ # 7: Change column names
+ setnames(refine_clean, old=c("old_name","another_old_name"), new=c("new_name", "another_new_name"))
+
+ # 8: Change value names
+ junk$nm <- as.character(junk$nm)
+ junk$nm[junk$nm == "B"] <- "b"
+
+  # EDIT: And if indeed you need to maintain nm as factors, add this in the end:
+ junk$nm <- as.factor(junk$nm)
+ 
+ setnames(refine_clean, old=c("old_name","another_old_name"), new=c("new_name", "another_new_name"))
+ 
+ products < read_csv("refine_clean")
+ 
